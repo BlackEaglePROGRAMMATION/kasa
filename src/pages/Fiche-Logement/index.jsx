7bccba4ede28ec7fd19e91c2991d124eee105f3a
Corentin Beaudet <corentin.beaudet14@gmail.com>
2023-11-08 13:20:04 +0100

    create fiche_logement page

--- src/pages/Fiche-Logement/index.jsx
+++ src/pages/Fiche-Logement/index.jsx
@@ -1,9 +1,53 @@
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import Slider from '../../components/Slider'
+import Profil from '../../components/Profil'
+import Rating from '../../components/Rating'
+import Collapse from '../../components/Collapse'
+
+import logements from "./../../data/logements"
 import "./index.css"
 
 function FicheLogement() {
+    const navigate = useNavigate()
+    const { logementId } = useParams()
+    const [slide, updateSlide] = useState(0)
+
+    const logement = logements.find(({ id }) => id === logementId)    
+
+    useEffect(() => {
+        if (!logement) {
+            navigate("*");
+            return
+        }
+    }, [logement, navigate]);
+
+    if (!logement) {
+        return null;
+    }
+
     return (
         <div className="fiche-logement">
-            
+            <div className="fl-container" key={logement.id}>
+                <Slider slide={slide} updateSlide={updateSlide} title={logement.title} pictures={logement.pictures} />
+
+                <div className="fl-divTitle">
+                    <h1>{logement.title}</h1>
+                    <p>{logement.location}</p>
+                    {logement.tags.map((tag) => (<span className='tag' key={tag}>{tag}</span>))}
+                </div>
+
+                <div className="fl-divNameRating">
+                    <Profil host={logement.host} />
+                    <Rating rating={logement.rating} />
+                </div>
+
+                <ul className="fl-details">
+                    <Collapse title="description" texte={logement.description} />
+                    <Collapse title="equipments" list={logement.equipments} />
+                </ul>
+            </div>
         </div>
     )
 }
